64a8c65ff6f739e019d201e0d416dd457751fe84
Hossein Sheikhi <hsa150@sfu.ca>
2020-05-22 16:59:29 -0700

    Modify pure Linear Regression for tutorial

--- PureLinearRegression.py
+++ PureLinearRegression.py
@@ -2,57 +2,60 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 
-rng = np.random
-
 # parameters
-learning_rate = 0.001
-training_steps = 1000
-display_step = 100
+learning_rate = 0.001  # alpha in gradient descent algorithm
+epoch = 500  # How many time iterate on the whole data
+log_counter = 100  # Log data every log_counter epoch
 
-# Training Data.
-X = np.array([3.3, 4.4, 5.5, 6.71, 6.93, 4.168, 9.779, 6.182, 7.59, 2.167,
-              7.042, 10.791, 5.313, 7.997, 5.654, 9.27, 3.1])
-Y = np.array([1.7, 2.76, 2.09, 3.19, 1.694, 1.573, 3.366, 2.596, 2.53, 1.221,
-              2.827, 3.465, 1.65, 2.904, 2.42, 2.94, 1.3])
+# Generate (x,y) data to predict
+X = np.arange(-5, 5, 0.5) + 2
+Y = X / 2 + 0.3 * np.random.standard_normal(size=(len(X),))  # Y = X/2 + some_noise
 
-# Weight and Bias, Initialized Randomly
-W = tf.Variable(initial_value=rng.randn(), trainable=True, name='weights')
-b = tf.Variable(initial_value=rng.randn(), trainable=True, name='bias')
+# Define and initialize Weight (or slope) and Bias (or intercept)
+random_initializer = tf.random_normal_initializer(mean=0, stddev=0.1)
+W = tf.Variable(initial_value=random_initializer([1], dtype=tf.float64), trainable=True)
+b = tf.Variable(initial_value=random_initializer([1], dtype=tf.float64), trainable=True)
 
 
-# Linear Regression (Wx+b)
+#  Linear regression is nothing just fit a line ( y_hat = W*x + b) by least possible error
 def linear_regression(x):
-    return W*x+b
+    return W * x + b
 
 
-# Mean Squared Error
+# define mean square error to calculate loss function
+@tf.function
 def mean_square(y_pred, y_true):
-    return tf.reduce_mean(tf.square(y_pred - y_true))
+    return tf.reduce_mean(tf.square(y_pred - y_true))  # make sure y_pred, y_true are in same data type
 
 
 optimizer = tf.optimizers.SGD(learning_rate)
 
 
-# Optimization process
+# fit a line by linear regression algorithm
+# calculate how much the line is a good fit
+# calculate gradients of objective (MSE) w.r.t weight and bias
+# use SGD optimizer to apply the gradient descent algorithm on weight and bias
+@tf.function
 def run_optimization():
     with tf.GradientTape() as g:
-        pred = linear_regression(X)
-        loss = mean_square(pred, Y)
+        y_hat = linear_regression(X)
+        loss = mean_square(y_hat, Y)
 
     # compute gradients
     gradients = g.gradient(loss, [W, b])
     optimizer.apply_gradients(zip(gradients, [W, b]))
 
 
-for step in range(training_steps):
+for step in range(epoch):
     run_optimization()
 
-    if step % display_step == 0:
-        pred = linear_regression(X)
-        loss = mean_square(pred, Y)
-        print(loss)
+    if (step+1) % log_counter == 0:
+        prediction = linear_regression(X)
+        loss = mean_square(prediction, Y)
+        print("epoch: {}  loss:{}".format(step+1, loss.numpy()))
 
-plt.plot(X, Y, 'ro', label='Original data')
-plt.plot(X, np.array(W * X + b), label='Fitted Line')
+
+plt.scatter(X, Y, label='Original data')
+plt.plot(X, prediction.numpy(), color='red', label='Fitted Line')
 plt.legend()
 plt.show()
